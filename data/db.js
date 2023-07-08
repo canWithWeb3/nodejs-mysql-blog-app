aebb5356124b0598d1ce41ad6ff41dce562882da
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-08 21:00:38 +0300

    sequelize-2

--- data/db.js
+++ data/db.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2")
 const config = require("../config")
 
 const Sequelize = require("sequelize")
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express;
+const router = express.Router();
 
 const imageUpload = require("../helpers/image-upload")
 
